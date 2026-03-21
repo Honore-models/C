4816c210656e9132a20c50341532324164d74c16
honore <honoreniyogushimwa63@gmail.com>
2026-03-21 17:30:57 +0200

    new

--- files/rewind.c
+++ files/rewind.c
@@ -3,9 +3,15 @@
 
 int main(){
 
+    int c;
+    char profile[30] = "I am a student";
+    printf("Enter a text: ");
+    for(int i=1; i<15; i++){
+        c = fgetc(stdin);
+        printf("%c",c);
+    }
 
-
-
+    printf("%c",c);
 
 return 0;
 }
