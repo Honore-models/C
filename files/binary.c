839c9f59a27a63acaaa66c9e3f367d66d66ea3cc
honore <honoreniyogushimwa63@gmail.com>
2026-05-13 20:36:33 +0200

    feat

--- files/binary.c
+++ files/binary.c
@@ -4,8 +4,9 @@ int main(){
 
     FILE *bin;
     int num = 10;
+    int num2;
 
-    bin = fopen("C:\\Users\\HP\\Desktop\\LANGUAGES\\C\\program.bin", "wb");
+    bin = fopen("C:\\Users\\HP\\Desktop\\LANGUAGES\\C\\program.bin", "wb+");
 
     if(bin == NULL){
         printf("Opening file failed.");
@@ -13,6 +14,10 @@ int main(){
     }
 
     fwrite(&num, sizeof(int), 1, bin);
+    rewind(bin);
+    fread(&num2, sizeof(int), 1, bin);
+
+    printf("The values read is %d", num2);
     fclose(bin);
 
     return 0;
