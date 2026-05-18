6e15f6b7eac56bc6754155328b90e4495b7bca04
honore <honoreniyogushimwa63@gmail.com>
2026-05-18 20:23:38 +0200

    feat(): finished exercise one on enum

--- files/enums.c
+++ files/enums.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+enum Day{
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    };
+
+int main(){
+
+    printf("Monday = %d\n", Monday);
+    printf("Tuesday = %d\n", Tuesday);
+    printf("Wednesday = %d\n", Wednesday);
+    printf("Thursday = %d\n", Thursday);
+    printf("Friday = %d\n", Friday);
+
+return 0;
+}
