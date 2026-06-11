19fc5ab889e795a781c1c9d583ffe8077d2c2d8b
honore <honoreniyogushimwa63@gmail.com>
2026-06-11 12:49:31 +0200

    added gitignore

---
+++ files/selection_sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+
+        // Find the minimum element in the remaining unsorted array
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap if a smaller element was found
+        if (minIndex != i) {
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {29, 10, 14, 37, 13};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before sorting: ");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("After sorting: ");
+    printArray(arr, n);
+
+    return 0;
+}
